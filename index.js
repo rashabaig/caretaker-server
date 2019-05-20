8e69a3f78cf3329542210eacbd774f69b9ca1d4f
Rasha Baig <rasha.baig@gmail.com>
2019-05-20 16:21:15 -0400

    resolved conflicts

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const appointmentController = require('./controllers/appointmentController');
 const medicationController = require('./controllers/medicationController');
 const doctorController = require('./controllers/doctorController');
 const dailyNoteController = require('./controllers/dailyNoteController');
-=======
-const bloodPressureController = require('./controllers/BloodPressureController')
-const weightController = require('./controllers/weightController')
->>>>>>> tanyasbranch
 
 const cors = require('cors');
 
@@ -21,15 +17,10 @@ app.use(cors());
 
 app.use('/', userController);
 app.use('/sugarlevel', sugarlevelController);
-<<<<<<< HEAD
 app.use('/appointment', appointmentController);
 app.use('/medication', medicationController);
 app.use('/doctor', doctorController);
 app.use('/note', dailyNoteController);
-=======
-app.use('/bloodpressure', bloodPressureController);
-app.use('/weight', weightController);
->>>>>>> tanyasbranch
 
 app.set('port', process.env.PORT || 3001);
 
