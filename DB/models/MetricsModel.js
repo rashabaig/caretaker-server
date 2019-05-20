fd687507938033821955707e94786e29ea69a8e8
Tetyana Radu <tanyaradu@Tanyas-MacBook-Pro.local>
2019-05-20 10:51:34 -0400

    Models: Metric, Weight, Pressure, Sugar

--- DB/models/MetricsModel.js
+++ DB/models/MetricsModel.js
@@ -1,1 +1,19 @@
 const mongoose = require('../../connection');
+
+var Metric = new mongoose.Schema({
+    sugarLevel:[{
+        type: Schema.Types.ObjectId,
+        ref: "SugarLevel"
+    }],
+    bloodPressure: [{
+        type: Schema.Types.ObjectId,
+        ref: "BloodPressure"
+    }],
+    weight: [{
+        type: Schema.Types.ObjectId,
+        ref: "Weight"
+    }]
+
+});
+
+module.exports = mongoose.model("Metric", Metric);
