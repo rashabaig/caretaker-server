f2352fa9b635cf0259197f2722cad57a95471fde
Tetyana Radu <tanyaradu@Tanyas-MacBook-Pro.local>
2019-05-20 15:33:37 -0400

    controllers: sugar, weight, bloodpressure

--- controllers/BloodPressureController.js
+++ controllers/BloodPressureController.js
@@ -1,5 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const BloodPressureModel = require('../DB/models/BloodPressureModel');
+const UserModel = require('../DB/models/UserModel');
+// create new bloodPressure -- works
+router.put('/:UserID', (req, res) => {
+	UserModel.findOneAndUpdate({ _id: req.params.UserID })
+		.then((user) => {
+			console.log(user);
+			BloodPressureModel.create(req.body).then((bloodPressureNew) => {
+				user.bloodPressure.push(bloodPressureNew._id);
+				bloodPressureNew.save();
+				console.log(user);
+			});
+			return res.sendStatus(200);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+// delete bloodPressure -- works
+router.delete('/:bloodPressureID', (req, res) => {
+	BloodPressureModel.findOneAndDelete({ _id: req.params.bloodPressureID })
+		.then(() => {
+			return res.sendStatus(200);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+// update bloodPressure -- works
+router.put('/update/:bloodPressureID', (req, res) => {
+	BloodPressureModel.findOneAndUpdate({ _id: req.params.bloodPressureID },{$set: req.body})
+		.then(() => {res.sendStatus(200)})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const parser = require('body-parser');
 const userController = require('./controllers/userController');
 const sugarlevelController = require('./controllers/sugarlevelController');
+const bloodPressureController = require('./controllers/BloodPressureController')
+const weightController = require('./controllers/weightController')
 
 const cors = require('cors');
 
@@ -12,6 +14,8 @@ app.use(cors());
 
 app.use('/', userController);
 app.use('/sugarlevel', sugarlevelController);
+app.use('/bloodpressure', bloodPressureController);
+app.use('/weight', weightController);
 
 app.set('port', process.env.PORT || 3001);
 
